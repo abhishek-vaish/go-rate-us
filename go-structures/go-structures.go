50cb2cd7909b63e1b4076b686d63b9347f429b71
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-08-02 09:52:05 +0530

    update structures

--- go-structures/go-structures.go
+++ go-structures/go-structures.go
@@ -3,23 +3,32 @@ package main
 import "fmt"
 
 type avengers struct {
-	name  string
-	power int
+	name         string
+	power        int
+	newStructure *avengers
 }
 
 func main() {
 	object := &avengers{
 		name:  "Iron Man",
 		power: 2000,
+		//fields of a structures
+		newStructure: &avengers{
+			name:  "Hulk Buster",
+			power: 8000,
+		},
 	}
-	captain := avengers{"Captain America", 5000}
+	captain := avengers{
+		name:  "Captain America",
+		power: 5000,
+	}
 	howkie := avengers{
 		name: "Howkie",
 	}
 	spiderMan := newAvengers("Spider Man", 1000)
 
 	updateIronMan(object)
-	fmt.Println("Avengers : ", object)
+	fmt.Println("Avengers : ", object.newStructure.name)
 	fmt.Println("Avengers : ", object.name)
 	fmt.Println("Avengers : ", captain.name)
 	fmt.Println("Avengers : ", howkie.name)
